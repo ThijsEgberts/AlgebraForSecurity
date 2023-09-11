b42d95eb72b59cce324ceadea48bbe298787e9e4
gijsnolet06@gmail.com <FUzL&un23^OJ9bG8>
2023-09-11 11:13:09 +0200

    Ahhh

--- solve.py
+++ solve.py
@@ -10,3 +10,25 @@ def solve_exercise(exercise_location : str, answer_location : str):
     save_answer(answer, answer_location)
     # Print the answer
     print(answer)
+
+def load_exercise(exercise_location : str):
+    """
+    Loads the exercise at the given location and returns a JSON object.
+    """
+    # Load the exercise file
+    exercise = load_exercise_file(exercise_location)
+    # Parse the exercise
+    exercise = parse_exercise(exercise)
+    # Return the exercise
+    return exercise
+
+def load_exercise_file(exercise_location : str):
+    """
+    Loads the exercise at the given location.
+    """
+    # Open the exercise file
+    with open(exercise_location, 'r') as exercise_file:
+        # Read the exercise file
+        exercise = exercise_file.read()
+    # Return the exercise
+    return exercise
