ed48c342a5dc4875e43d2e392118099d3637816c
gijsnolet06@gmail.com <FUzL&un23^OJ9bG8>
2023-09-11 11:21:14 +0200

    Implemented de basics :)

--- solve.py
+++ solve.py
@@ -4,15 +4,29 @@ def solve_exercise(exercise_location : str, answer_location : str):
     """
     Solves the exercise at the given location and saves the answer at the given location.
     """
-    # Load the exercise and return the JSON object with fields:
-    # type, operation, radix, x, points
+    # Load the exercise and return the JSON object (See Assignment 2.3)
     exercise = load_exercise(exercise_location)
-    # Solve the exercise
+    # Solve the exercise and returns a JSON object with the answer(s) (see Assignment 2.4)
     answer = solve(exercise)
     # Save the answer
     save_answer(answer, answer_location)
     # Print the answer
     print(answer)
+
+def solve(exercise : dict):
+    """
+    Solve chooses the correct solver for the chosen operation and returns the answer.
+    """
+    #TODO: Implement solve
+
+def save_answer(answer : dict, answer_location : str):
+    """
+    Saves the answer at the given location.
+    """
+    # Open the answer file
+    with open(answer_location, 'w') as answer_file:
+        # Write the answer file
+        answer_file.write(json.dumps(answer))
 
 def load_exercise(exercise_location : str):
     """
