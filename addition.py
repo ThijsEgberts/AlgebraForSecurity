11865da640d497b6ecff38b8d6dd41f8978a0c11
gijsnolet06@gmail.com <FUzL&un23^OJ9bG8>
2023-09-11 17:25:24 +0200

    Added comments and TODO's

--- addition.py
+++ addition.py
@@ -1,6 +1,10 @@
 import BigNumber
 import substraction
 
+#TODO: Ask teacher, can we use standart python addition at all, or should we implement it ourselves with a switch for addition cases?
+#TODO: Also calculate time complexity and see if it matches the theoretical time complexity.
+#TODO: Unit Tests :happy:
+
 def solve_addition(type : str, radix : int, x : str, y : str):
     BigNumberX = BigNumber(x, radix)
     BigNumberY = BigNumber(y, radix)
@@ -16,7 +20,16 @@ def solve_addition_integer_arithmetic(type : str, originalX : str, originalY : s
     #TODO: use use 32 bit types, this is mandated by the assignment
     """
     Solves the addition of two numbers in integer arithmetic.
+
+    The algorithm is as follows:
+    1. If the signs are different, we need to subtract the smaller number from the bigger number and return the result with the sign of the bigger number.
+    2. If the signs are the same, we need to add the numbers starting with the last exponent and carry the 1 if needed.
+    3. If there is a carry left, we need to add it to the exponents.
+    4. Calculate a number using the exponent list. Using number * radix ** exponent. So [1, 1, 1] would be 1*2^2 + 1*2^1 + 1*2^0 = 7
+    5. Return the result.
     """
+
+    #1.
     #If the signs are different, we need to subtract the smaller number from the bigger number
     if x.isNegative != y.isNegative:
         if x.isNegative == 0:
@@ -24,6 +37,7 @@ def solve_addition_integer_arithmetic(type : str, originalX : str, originalY : s
         else:
             return substraction.solve_subtraction(type, y, x)
 
+    #2.
     #If the signs are the same, we need to add the numbers starting with the last exponent and carry the 1 if needed
     exponents = []
     carry = 0
@@ -39,20 +53,22 @@ def solve_addition_integer_arithmetic(type : str, originalX : str, originalY : s
             #Carry the 1
             carry = 1
 
+    #3.
     #If there is a carry left, we need to add it to the exponents
     if carry == 1:
         exponents.insert(0, 1)
     
+    #4.
     #Calculate a number using the exponent list
     # Using number * radix ** exponent
     # so [1, 1, 1] would be 1*2^2 + 1*2^1 + 1*2^0 = 7
-
     result = 0
     exponent = len(exponents) - 1
     for i in range(len(exponents)):
         result += exponents[i] * x.radix ** exponent
         exponent -= 1
 
+    #5.
     return result
 
 def solve_addition_modular_arithmetic(x : BigNumber, y : BigNumber):
