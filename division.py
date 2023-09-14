1cda1d509e9e840bd8243d8c800fd27a305ddb0c
gijsnolet06@gmail.com <FUzL&un23^OJ9bG8>
2023-09-14 15:03:51 +0200

    Finished solve_division_with_remainder

--- division.py
+++ division.py
@@ -1,6 +1,7 @@
 from BigNumber import BigNumber
 import BigNumber as bn
 import substraction
+import addition
 
 def solve_division_with_remainder(x : BigNumber, y : BigNumber):
     """
@@ -15,21 +16,12 @@ def solve_division_with_remainder(x : BigNumber, y : BigNumber):
     This is based on Euclid's algorithm.
     """
     quotient = BigNumber("0", x.radix)
+
     while bn.isGreaterOrEqual(x, y):
         # Calculate the remainder after subtracting y from x
         x = BigNumber(substraction.solve_subtraction_integer_arithmetic(x, y), x.radix)
-        # TODO Add 1 to the quotient
-        
-
-
-    # TODO remove once finished
-    # function euclidsAlgorithmDivision(a, b)
-        # while a ≥ b
-        #     a = a - b
-        # end while
-    #     return a
-    # end function
-
+        # Add 1 to the quotient
+        quotient = BigNumber(addition.solve_addition_integer_arithmetic(quotient, BigNumber("1", x.radix)), x.radix)
 
     # Result contains the quotient and the remainder in form [quotient, remainder]
     result = [quotient, x]
