c72e9cfaf4d84b14310d0c89420c5eada7cb81c0
ThijsEgberts <t.t.a.egberts@student.tue.nl>
2023-09-22 17:01:55 +0200

    Removed a comment from reduction

--- reduction.py
+++ reduction.py
@@ -2,16 +2,7 @@ from fixedint import Int32
 import BigNumber
 import division
 
-#solves a reduction x modulo mod with string input
-# def solve_reduction(radix : Int32, x : str, mod : str):
-#     if mod == "0":
-#         return None
-    
-#     x = BigNumber.BigNumber(x, radix)
-#     mod = BigNumber.BigNumber(mod, radix)
-#     return reduce_number(x, mod).exponentsToString()
-
-#reduces x modolu mod
+#Solves a modular reduction
 def solve_reduction(x : BigNumber, mod : BigNumber) -> BigNumber:
     if str(mod) == "0":
         return None
