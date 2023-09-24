0ca64aa7697e3786fd4949ecd8aae98722fa299c
ThijsEgberts <t.t.a.egberts@student.tue.nl>
2023-09-24 18:28:16 +0200

    made a mostly working version of eea

--- extended_euclidean_algorithm.py
+++ extended_euclidean_algorithm.py
@@ -1,11 +1,15 @@
-from BigNumber import BigNumber
+from BigNumber import BigNumber, createBigNumberFromExponents
 from division import solve_division_with_remainder
 from multiplication_karatsuba import solve_multiplication_karatsuba
-from addition_subtraction import solve_subtraction_integer_arithmetic
+from addition_subtraction import solve_addition_integer_arithmetic, solve_subtraction_integer_arithmetic
 from fixedint import Int32
 
+from multiplication_primary import solve_multiplication_primary
+
 #source: https://www.baeldung.com/cs/extended-euclidean-algorithm
-def solve_extended_euclidean(a: BigNumber, b: BigNumber) -> (BigNumber, BigNumber, BigNumber):
+def solve_extended_euclidean(a_: BigNumber, b_: BigNumber) -> (BigNumber, BigNumber, BigNumber):
+    a = createBigNumberFromExponents(a_.radix, a_.exponents, a_.isNegative)
+    b = createBigNumberFromExponents(b_.radix, b_.exponents, b_.isNegative)
     #when a or b is 0, the other is the gcd
     if str(a) == "0":
         return b, BigNumber("0", a.radix), BigNumber("1", a.radix)
@@ -13,35 +17,49 @@ def solve_extended_euclidean(a: BigNumber, b: BigNumber) -> (BigNumber, BigNumbe
         return a, BigNumber("0", a.radix), BigNumber("1", a.radix)
     
     #gcd(a, b) = gcd(|a|, |b|)
-    a.setSign(0) #makes a positive
-    b.setSign(0)
+    # a.setSign(0) #makes a positive
+    # b.setSign(0)
     
     #make sure a >= b
     if not a.compare(b, False):
         b, a = a, b    
     
-    x2, x1, y2, y1 = BigNumber("1", a.radix), BigNumber("0", a.radix), BigNumber("0", a.radix), BigNumber("1", a.radix)
+    x, x1, y, y1 = BigNumber("1", a.radix), BigNumber("0", a.radix), BigNumber("0", a.radix), BigNumber("1", a.radix)
     
     while str(b) != "0":
+        # print("new iter")
         q, r = solve_division_with_remainder(a, b)
+        # print(str(a), str(b))
         
-        x = solve_subtraction_integer_arithmetic(x1, solve_multiplication_karatsuba(q,x2))
-        y = solve_subtraction_integer_arithmetic(y1, solve_multiplication_karatsuba(q,y2))
+        # qx2 = solve_multiplication_primary(q,x1)
+        # print("qx2 =", str(q), "*", str(x2), "=", str(qx2))
+        # x = solve_subtraction_integer_arithmetic(x2, qx2)
+        # qy2 = solve_multiplication_primary(q,y1)
+        # print("qy2 =", str(q), "*", str(y2), "=", str(qy2))
+        # y = solve_subtraction_integer_arithmetic(y2, qy2)
+        x, x1 = x1, solve_subtraction_integer_arithmetic(x, solve_multiplication_karatsuba(q,x1))
+        y, y1 = y1, solve_subtraction_integer_arithmetic(y, solve_multiplication_karatsuba(q,y1))
         
-        x2 = x1
-        x1 = x
-        y2 = y1
-        y1 = y
+        # print("q:", str(q), "r:", str(r), "x:", str(x), "x1:", str(x1), "x2:", str(x2), "y:", str(y), "y1:", str(y1), "y2:", str(y2))
+        
+        # x2 = x1
+        # x1 = x
+        # y2 = y1
+        # y1 = y
         a = b
         b = r
-        
+        # print("q:", str(q), "r:", str(r), "x:", str(x), "x1:", str(x1), "y:", str(y), "y1:", str(y1))
+        # print(str(a), str(b), str(x), str(y))
     gcd = a
     # gcd, x1, y1 = solve_extended_euclidean(solve_division_with_remainder(b, a)[1], a)
     
     # x = solve_subtraction_integer_arithmetic(y1, solve_multiplication_karatsuba((solve_division_with_remainder(b, a)[0]), x1))
     # y = x1
     
-    return gcd, x, y
+    #test
+    # if str(gcd) == str(solve_addition_integer_arithmetic(solve_multiplication_karatsuba(a_, y), solve_multiplication_karatsuba(b_,x))):
+    #     print(str(gcd),"=", str(solve_addition_integer_arithmetic(solve_multiplication_karatsuba(a_, y), solve_multiplication_karatsuba(b_,x))))
+    return gcd, y, x
 
 
 # https://brilliant.org/wiki/extended-euclidean-algorithm/
@@ -69,3 +87,8 @@ def solve_euclidean_algorithm(x, y):
         return solve_euclidean_algorithm(y, r)
     else: 
         return solve_euclidean_algorithm(x,r)
+
+# gcd_, x_, y_ = solve_extended_euclidean(BigNumber("254", Int32(10)), BigNumber("44", Int32(10)))
+# print("GCD " + str(gcd_))
+# print("x " + str(x_))
+# print("y " + str(y_))
