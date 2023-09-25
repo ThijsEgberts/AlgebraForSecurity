566866dcca9d270b7eaa3c78163f2c0acbcf1650
gijsnolet06@gmail.com <FUzL&un23^OJ9bG8>
2023-09-25 13:02:08 +0200

    Optimizations

--- multiplication_primary.py
+++ multiplication_primary.py
@@ -1,11 +1,10 @@
 from BigNumber import BigNumber
-from fixedint import Int32
 import addition_subtraction
 
 # Code taken from https://stackoverflow.com/questions/2267362/how-to-convert-an-integer-to-a-string-in-any-base
 
 
-def numberToBase(n: Int32, b: Int32):
+def numberToBase(n: int, b: int):
     if n == 0:
         return [0]
     digits = []
@@ -33,7 +32,7 @@ def solve_multiplication_primary(x: BigNumber, y: BigNumber) -> BigNumber:
 
     # Initialize variables for exponents multiplication
     result = BigNumber(x.radix, [0], False)
-    carry = Int32(0)
+    carry = 0
 
     # Multiply exponents from right to left
     for i in range(len(x.exponents) - 1, -1, -1):
@@ -41,25 +40,28 @@ def solve_multiplication_primary(x: BigNumber, y: BigNumber) -> BigNumber:
 
         # Fill the result with zeros corresponding to the position of the exponent
         for _ in range(len(x.exponents) - 1 - i):
-            exponents_result.append(Int32(0))
+            exponents_result.append(0)
 
         for j in range(len(y.exponents) - 1, -1, -1):
             product = x.exponents[i] * y.exponents[j] + carry
 
             if product < x.radix:
                 exponents_result.insert(0, product)
-                carry = Int32(0)
+                carry = 0
             else:
-                result_big_number = BigNumber(
-                    Int32(x.radix), numberToBase(Int32(product), Int32(x.radix)), False)
+                result_number = numberToBase(product, x.radix)
 
-                exponents_result.insert(0, result_big_number.exponents[-1])
-                carry = Int32(result_big_number.exponents[0])
+                exponents_result.insert(0, result_number[-1])
+                carry = result_number[0]
 
         # If there is a carry left, add it to the result
-        if carry != Int32(0):
+        if carry != 0:
             exponents_result.insert(0, carry)
 
-        carry = Int32(0)
-        result = addition_subtraction.solve_addition_integer_arithmetic(result, BigNumber(x.radix, exponents_result, is_negative))
+        carry = 0
+        result = addition_subtraction.solve_addition_integer_arithmetic(
+            result, BigNumber(x.radix, exponents_result, is_negative))
     return result
+
+
+# print(numberToBase(Int32(230), Int32(16)))
