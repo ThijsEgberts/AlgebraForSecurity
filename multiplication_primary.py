e9f4d8e434d6c92427d244362aedf4ef98183e9a
gijsnolet06@gmail.com <FUzL&un23^OJ9bG8>
2023-09-25 12:04:09 +0200

    Optimized additionlogic in multiplication

--- multiplication_primary.py
+++ multiplication_primary.py
@@ -4,7 +4,9 @@ from fixedint import Int32
 import addition_subtraction
 
 # Code taken from https://stackoverflow.com/questions/2267362/how-to-convert-an-integer-to-a-string-in-any-base
-def numberToBase(n : Int32, b : Int32):
+
+
+def numberToBase(n: Int32, b: Int32):
     if n == 0:
         return [0]
     digits = []
@@ -12,6 +14,7 @@ def numberToBase(n : Int32, b : Int32):
         digits.append(int(n % b))
         n //= b
     return digits[::-1]
+
 
 def solve_multiplication_primary(x: BigNumber, y: BigNumber) -> BigNumber:
     """
@@ -27,10 +30,10 @@ def solve_multiplication_primary(x: BigNumber, y: BigNumber) -> BigNumber:
     """
 
     # Determine the sign of the result
-    is_negative = int(x.isNegative != y.isNegative)
+    is_negative = x.isNegative ^ y.isNegative
 
     # Initialize variables for exponents multiplication
-    exponents_multiplication_results = []
+    result = createBigNumberFromExponents(x.radix, [0], False)
     carry = Int32(0)
 
     # Multiply exponents from right to left
@@ -48,7 +51,8 @@ def solve_multiplication_primary(x: BigNumber, y: BigNumber) -> BigNumber:
                 exponents_result.insert(0, product)
                 carry = Int32(0)
             else:
-                result_big_number = createBigNumberFromExponents(Int32(x.radix), numberToBase(Int32(product), Int32(x.radix)), False)
+                result_big_number = createBigNumberFromExponents(
+                    Int32(x.radix), numberToBase(Int32(product), Int32(x.radix)), False)
 
                 exponents_result.insert(0, result_big_number.exponents[-1])
                 carry = Int32(result_big_number.exponents[0])
@@ -58,19 +62,5 @@ def solve_multiplication_primary(x: BigNumber, y: BigNumber) -> BigNumber:
             exponents_result.insert(0, carry)
 
         carry = Int32(0)
-        exponents_multiplication_results.append(exponents_result)
-
-    # Convert the list of lists to a list of BigNumbers
-    exponents_multiplication_results_big_numbers = [
-        createBigNumberFromExponents(x.radix, exponents, is_negative) for exponents in exponents_multiplication_results
-    ]
-    
-    # Add the BigNumbers together
-    result = BigNumber("0", x.radix)
-    for exponents_big_number in exponents_multiplication_results_big_numbers:
-        result = addition_subtraction.solve_addition_integer_arithmetic(
-            result, exponents_big_number)
-    # Set the sign of the result
-    result.isNegative = is_negative
-
+        result = addition_subtraction.solve_addition_integer_arithmetic(result, createBigNumberFromExponents(x.radix, exponents_result, is_negative))
     return result
