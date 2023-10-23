1e40de0cab7e73da8ad141698c2de2e0ce8e46c6
ThijsEgberts <t.t.a.egberts@student.tue.nl>
2023-10-23 17:29:13 +0200

    updated solve.py and added a test suite for the examples given by the course

--- run_test_cases.py
+++ run_test_cases.py
@@ -4,127 +4,127 @@ import solve
 
 
 def runSimplePolyAddSub():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise0.json",
-                         "exercises\Simple\Answers\\answer0.json", False)
-    solve.solve_exercise("exercises\Simple\Exercises\exercise1.json",
-                         "exercises\Simple\Answers\\answer1.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise0.json",
+                         "Examples\Simple\Answers\\answer0.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise1.json",
+                         "Examples\Simple\Answers\\answer1.json", False)
 
 def runSimplePolyMulti():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise2.json",
-                         "exercises\Simple\Answers\\answer2.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise2.json",
+                         "Examples\Simple\Answers\\answer2.json", False)
 
 def runSimplePolyLongDivision():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise3.json",
-                         "exercises\Simple\Answers\\answer3.json", False)
-    solve.solve_exercise("exercises\Simple\Exercises\exercise4.json",
-                         "exercises\Simple\Answers\\answer4.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise3.json",
+                         "Examples\Simple\Answers\\answer3.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise4.json",
+                         "Examples\Simple\Answers\\answer4.json", False)
 
 def runSimplePolyEucl():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise5.json",
-                         "exercises\Simple\Answers\\answer5.json", False)
-    solve.solve_exercise("exercises\Simple\Exercises\exercise6.json",
-                         "exercises\Simple\Answers\\answer6.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise5.json",
+                         "Examples\Simple\Answers\\answer5.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise6.json",
+                         "Examples\Simple\Answers\\answer6.json", False)
 
 def runSimplePolyIrreducibilityCheck():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise7.json",
-                         "exercises\Simple\Answers\\answer7.json", False)
-    solve.solve_exercise("exercises\Simple\Exercises\exercise8.json",
-                         "exercises\Simple\Answers\\answer8.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise7.json",
+                         "Examples\Simple\Answers\\answer7.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise8.json",
+                         "Examples\Simple\Answers\\answer8.json", False)
 
 def runSimplePolyIrreducibleGenerator():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise9.json",
-                         "exercises\Simple\Answers\\answer9.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise9.json",
+                         "Examples\Simple\Answers\\answer9.json", False)
     
 def runSimpleFFAddSub():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise10.json",
-                         "exercises\Simple\Answers\\answer10.json", False)
-    solve.solve_exercise("exercises\Simple\Exercises\exercise11.json",
-                         "exercises\Simple\Answers\\answer11.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise10.json",
+                         "Examples\Simple\Answers\\answer10.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise11.json",
+                         "Examples\Simple\Answers\\answer11.json", False)
     
 def runSimpleFFMulti():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise12.json",
-                         "exercises\Simple\Answers\\answer12.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise12.json",
+                         "Examples\Simple\Answers\\answer12.json", False)
     
 def runSimpleFFdivision():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise13.json",
-                         "exercises\Simple\Answers\\answer13.json", False)
-    solve.solve_exercise("exercises\Simple\Exercises\exercise14.json",
-                         "exercises\Simple\Answers\\answer14.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise13.json",
+                         "Examples\Simple\Answers\\answer13.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise14.json",
+                         "Examples\Simple\Answers\\answer14.json", False)
     
 def runSimpleFFinversion():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise15.json",
-                         "exercises\Simple\Answers\\answer15.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise15.json",
+                         "Examples\Simple\Answers\\answer15.json", False)
     
 def runSimpleFFPrimitivityCheck():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise16.json",
-                         "exercises\Simple\Answers\\answer16.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise16.json",
+                         "Examples\Simple\Answers\\answer16.json", False)
     
 def runSimpleFFPrimitiveGenerator():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise17.json",
-                         "exercises\Simple\Answers\\answer17.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise17.json",
+                         "Examples\Simple\Answers\\answer17.json", False)
     
     
     
 # Realistic test cases
 def runRealisticPolyAddSub():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise3.json",
-                         "exercises\Simple\Answers\\answer3.json", False)
-    solve.solve_exercise("exercises\Simple\Exercises\exercise15.json",
-                         "exercises\Simple\Answers\\answer15.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise3.json",
+                         "Examples\Simple\Answers\\answer3.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise15.json",
+                         "Examples\Simple\Answers\\answer15.json", False)
 
 def runRealisticPolyMulti():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise13.json",
-                         "exercises\Simple\Answers\\answer13.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise13.json",
+                         "Examples\Simple\Answers\\answer13.json", False)
 
 def runRealisticPolyLongDivision():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise2.json",
-                         "exercises\Simple\Answers\\answer2.json", False)
-    solve.solve_exercise("exercises\Simple\Exercises\exercise9.json",
-                         "exercises\Simple\Answers\\answer9.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise2.json",
+                         "Examples\Simple\Answers\\answer2.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise9.json",
+                         "Examples\Simple\Answers\\answer9.json", False)
 
 def runRealisticPolyEucl():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise11.json",
-                         "exercises\Simple\Answers\\answer11.json", False)
-    solve.solve_exercise("exercises\Simple\Exercises\exercise16.json",
-                         "exercises\Simple\Answers\\answer16.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise11.json",
+                         "Examples\Simple\Answers\\answer11.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise16.json",
+                         "Examples\Simple\Answers\\answer16.json", False)
 
 def runRealisticPolyIrreducibilityCheck():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise6.json",
-                         "exercises\Simple\Answers\\answer6.json", False)
-    solve.solve_exercise("exercises\Simple\Exercises\exercise14.json",
-                         "exercises\Simple\Answers\\answer14.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise6.json",
+                         "Examples\Simple\Answers\\answer6.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise14.json",
+                         "Examples\Simple\Answers\\answer14.json", False)
 
 def runRealisticPolyIrreducibleGenerator():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise4.json",
-                         "exercises\Simple\Answers\\answer4.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise4.json",
+                         "Examples\Simple\Answers\\answer4.json", False)
     
 def runRealisticFFAddSub():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise1.json",
-                         "exercises\Simple\Answers\\answer1.json", False)
-    solve.solve_exercise("exercises\Simple\Exercises\exercise10.json",
-                         "exercises\Simple\Answers\\answer10.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise1.json",
+                         "Examples\Simple\Answers\\answer1.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise10.json",
+                         "Examples\Simple\Answers\\answer10.json", False)
     
 def runRealisticFFMulti():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise12.json",
-                         "exercises\Simple\Answers\\answer12.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise12.json",
+                         "Examples\Simple\Answers\\answer12.json", False)
     
 def runRealisticFFdivision():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise0.json",
-                         "exercises\Simple\Answers\\answer0.json", False)
-    solve.solve_exercise("exercises\Simple\Exercises\exercise8.json",
-                         "exercises\Simple\Answers\\answer8.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise0.json",
+                         "Examples\Simple\Answers\\answer0.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise8.json",
+                         "Examples\Simple\Answers\\answer8.json", False)
     
 def runRealisticFFinversion():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise5.json",
-                         "exercises\Simple\Answers\\answer5.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise5.json",
+                         "Examples\Simple\Answers\\answer5.json", False)
     
 def runRealisticFFPrimitivityCheck():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise7.json",
-                         "exercises\Simple\Answers\\answer7.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise7.json",
+                         "Examples\Simple\Answers\\answer7.json", False)
     
 def runRealisticFFPrimitiveGenerator():
-    solve.solve_exercise("exercises\Simple\Exercises\exercise17.json",
-                         "exercises\Simple\Answers\\answer17.json", False)
+    solve.solve_exercise("Examples\Simple\Exercises\exercise17.json",
+                         "Examples\Simple\Answers\\answer17.json", False)
 
 
 # Runs all simple test cases provided by the lecturer
@@ -171,3 +171,5 @@ def runAll():
     runSimple()
     runRealistic()
     print("All test cases done.\n")
+    
+runSimplePolyAddSub()
--- solve.py
+++ solve.py
@@ -11,20 +11,52 @@ import finitefield_inversion
 import finitefield_multiplication
 import finitefield_primitive_element_generator
 import finitefield_primitivity_check
+from Polynomial import Polynomial
 
 
-def solve_exercise(exercise_location: str, answer_location: str):
+def solve_exercise(exercise_location: str, answer_location: str, realScenario=True):
     """
     Solves the exercise at the given location and saves the answer at the given location.
     """
     # Load the exercise and return the JSON object (See Assignment 2.3)
     exercise = load_exercise(exercise_location)
 
-    # Solve the exercise and returns a JSON object with the answer(s) (see Assignment 2.4)
+    # Solve the exercise and returns an object with the answer(s) (see Assignment 2.4)
     answer = solve(exercise)
 
     # Save the answer
-    save_answer(answer, answer_location, exercise["operation"])
+    if realScenario:
+        save_answer(answer, answer_location, exercise["operation"])
+    else:
+        answerDict = load_answer(answer_location)
+        match exercise:
+            case {'task' : 'long_division'}:
+                if answerDict['answer-q'] == answer[0].coefficients and answerDict['answer-r'] == answer[1].coefficients:
+                    print("Exercise solved correctly")
+                else:
+                    print("Exercise solved incorrectly:")
+                    print("Answer-q:", str(answer[0]))
+                    print("Target answer-q:", str(answerDict['answer-q']))
+                    print("Answer-r:", str(answer[1]))
+                    print("Target answer-r:", str(answerDict['answer-r']))
+            case {'task' : 'extended_euclidean_algorithm'}:
+                if answerDict['answer-a'] == answer[1].coefficients and answerDict['answer-b'] == answer[2].coefficients and answerDict['answer-gcd'] == answer[0].coefficients:
+                    print("Exercise solved correctly")
+                else:
+                    print("Exercise solved incorrectly:")
+                    print("Answer-a:", str(answer[1]))
+                    print("Target answer-a:", str(answerDict['answer-a']))
+                    print("Answer-b:", str(answer[2]))
+                    print("Target answer-b:", str(answerDict['answer-b']))
+                    print("Answer-gcd:", str(answer[0]))
+                    print("Target answer-gcd:", str(answerDict['answer-gcd']))
+            case _:        
+                if answerDict['answer'] == answer.coefficients:
+                    print("Exercise solved correctly")
+                else:
+                    print("Exercise solved incorrectly:")
+                    print("Answer:", str(answer))
+                    print("Target answer:", str(answerDict['answer']))
 
 
 def solve(exercise: dict) -> str:
@@ -34,53 +66,53 @@ def solve(exercise: dict) -> str:
     match exercise:
         case {'task': 'addition'}:
             if (exercise['type'] == 'polynomial_arithmetic'):
-                return polynomial_addition_subtraction.solve_addition_polynomial_arithmetic(exercise['f'], exercise['g'])
+                return polynomial_addition_subtraction.solve_addition_polynomial_arithmetic(Polynomial(exercise['integer_modulus'], exercise['f']), Polynomial(exercise['integer_modulus'], exercise['g']))
             if (exercise['type'] == "finite_field_arithmetic"):
-                return finitefield_addition_subtraction.solve_addition_finite_field_arithmetic(exercise['f'], exercise['g'], exercise['polynomial_modulus'])
+                return finitefield_addition_subtraction.solve_addition_finite_field_arithmetic(Polynomial(exercise['integer_modulus'], exercise['f']), Polynomial(exercise['integer_modulus'], exercise['g']), Polynomial(exercise['integer_modulus'], exercise['polynomial_modulus']))
 
         case {'task': 'subtraction'}:
             if (exercise['type'] == 'polynomial_arithmetic'):
-                return polynomial_addition_subtraction.solve_subtraction_polynomial_arithmetic(exercise['f'], exercise['g'])
+                return polynomial_addition_subtraction.solve_subtraction_polynomial_arithmetic(Polynomial(exercise['integer_modulus'], exercise['f']), Polynomial(exercise['integer_modulus'], exercise['g']))
             if (exercise['type'] == "finite_field_arithmetic"):
-                return finitefield_addition_subtraction.solve_subtraction_finite_field_arithmetic(exercise['f'], exercise['g'], exercise['polynomial_modulus'])
+                return finitefield_addition_subtraction.solve_subtraction_finite_field_arithmetic(Polynomial(exercise['integer_modulus'], exercise['f']), Polynomial(exercise['integer_modulus'], exercise['g']), Polynomial(exercise['integer_modulus'], exercise['polynomial_modulus']))
 
         case {'task': 'multiplication'}:
             if (exercise['type'] == 'polynomial_arithmetic'):
-                return polynomial_multiplication.solve_multiplication_polynomial_arithmetic(exercise['f'], exercise['g'])
+                return polynomial_multiplication.solve_multiplication_polynomial_arithmetic(Polynomial(exercise['integer_modulus'], exercise['f']), Polynomial(exercise['integer_modulus'], exercise['g']))
             if (exercise['type'] == "finite_field_arithmetic"):
-                return finitefield_multiplication.solve_multiplication_finite_field_arithmetic(exercise['f'], exercise['g'], exercise['polynomial_modulus'])
+                return finitefield_multiplication.solve_multiplication_finite_field_arithmetic(Polynomial(exercise['integer_modulus'], exercise['f']), Polynomial(exercise['integer_modulus'], exercise['g']), Polynomial(exercise['integer_modulus'], exercise['polynomial_modulus']))
 
         case {'task': 'long_division'}:
             if (exercise['type'] == 'polynomial_arithmetic'):
-                return polynomial_long_division.solve_long_division_polynomial_arithmetic(exercise['f'], exercise['g'])
+                return polynomial_long_division.solve_long_division_polynomial_arithmetic(Polynomial(exercise['integer_modulus'], exercise['f']), Polynomial(exercise['integer_modulus'], exercise['g']))
 
         case {'task': 'extended_euclidean_algorithm'}:
             if (exercise['type'] == 'polynomial_arithmetic'):
-                return polynomial_extended_euclidean_algorithm.solve_extended_euclidean_algorithm_polynomial_arithmetic(exercise['f'], exercise['g'])
+                return polynomial_extended_euclidean_algorithm.solve_extended_euclidean_algorithm_polynomial_arithmetic(Polynomial(exercise['integer_modulus'], exercise['f']), Polynomial(exercise['integer_modulus'], exercise['g']))
 
         case {'task': 'irreducibility_check'}:
             if (exercise['type'] == 'polynomial_arithmetic'):
-                return polynomial_irreducability_check.solve_irreducability_check_polynomial_arithmetic(exercise['f'], exercise['polynomial_modulus'])
+                return polynomial_irreducability_check.solve_irreducability_check_polynomial_arithmetic(Polynomial(exercise['integer_modulus'], exercise['f']))
 
         case {'task': 'irreducable_element_generator'}:
             if (exercise['type'] == 'polynomial_arithmetic'):
-                return polynomial_irreducable_element_generator.solve_irreducable_element_generator_polynomial_arithmetic(exercise['f'], exercise['polynomial_modulus'])
+                return polynomial_irreducable_element_generator.solve_irreducable_element_generator_polynomial_arithmetic(exercise['degree'], exercise['integer_modulus'])
 
         case {'task': 'division'}:
             if (exercise['type'] == 'finite_field_arithmetic'):
-                return finitefield_division.solve_division_finite_field_arithmetic(exercise['f'], exercise['g'], exercise['polynomial_modulus'])
+                return finitefield_division.solve_division_finite_field_arithmetic(Polynomial(exercise['integer_modulus'], exercise['f']), Polynomial(exercise['integer_modulus'], exercise['g']), Polynomial(exercise['integer_modulus'], exercise['polynomial_modulus']))
 
         case {'task': 'inversion'}:
             if (exercise['type'] == 'finite_field_arithmetic'):
-                return finitefield_inversion.solve_inversion_finite_field_arithmetic(exercise['f'], exercise['polynomial_modulus'])
+                return finitefield_inversion.solve_inversion_finite_field_arithmetic(Polynomial(exercise['integer_modulus'], exercise['f']), Polynomial(exercise['integer_modulus'], exercise['polynomial_modulus']))
 
         case {'task': 'primitivity_check'}:
             if (exercise['type'] == 'finite_field_arithmetic'):
-                return finitefield_primitivity_check.solve_primitivity_check_finite_field_arithmetic(exercise['f'], exercise['polynomial_modulus'])
+                return finitefield_primitivity_check.solve_primitivity_check_finite_field_arithmetic(Polynomial(exercise['integer_modulus'], exercise['f']), Polynomial(exercise['integer_modulus'], exercise['polynomial_modulus']))
 
         case {'task': 'primitive_element_generator'}:
             if (exercise['type'] == 'finite_field_arithmetic'):
-                return finitefield_primitive_element_generator.solve_primitive_element_generator_finite_field_arithmetic(exercise['f'], exercise['polynomial_modulus'])
+                return finitefield_primitive_element_generator.solve_primitive_element_generator_finite_field_arithmetic(Polynomial(exercise['integer_modulus'], exercise['polynomial_modulus']))
 
         # Invalid operation
         case _:
@@ -91,19 +123,19 @@ def solve(exercise: dict) -> str:
 def save_answer(answer, answer_location: str, operation: str):
     """
     Saves the answer at the given location. 
-    Using the JSON format "answer": "int".
+    Using the JSON format "answer": "coefficient list".
     """
     # Create the answer object
     if operation == "extended_euclidean_algorithm":
         answer_object = {
-            "answer-a": answer[1], "answer-b": answer[2], "answer-gcd": answer[0]}
+            "answer-a": str(answer[1]), "answer-b": str(answer[2]), "answer-gcd": str(answer[0])}
 
     elif operation == "long_division":
         answer_object = {
-            "answer-q": answer[0], "answer-r": answer[1]
+            "answer-q": str(answer[0]), "answer-r": str(answer[1])
         }
     else:
-        answer_object = {"answer": answer}
+        answer_object = {"answer": str(answer)}
 
     # Save the answer object to a JSON file
     with open(answer_location, 'w') as answer_file:
