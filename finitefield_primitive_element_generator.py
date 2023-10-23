0753f325bead087464e95ff2a7fcfc1b5226a499
ThijsEgberts <t.t.a.egberts@student.tue.nl>
2023-10-23 17:29:45 +0200

    added skeleton functions

---
+++ finitefield_primitive_element_generator.py
@@ -0,0 +1,5 @@
+from Polynomial import Polynomial
+
+def solve_primitive_element_generator_finite_field_arithmetic(polyMod: Polynomial):
+    intMod = polyMod.modulo
+    return
--- polynomial_irreducable_element_generator.py
+++ polynomial_irreducable_element_generator.py
@@ -1,1 +1,3 @@
-2
+def solve_irreducable_element_generator_polynomial_arithmetic(degree: int, integer_modulus: int):
+    #check input
+    return
