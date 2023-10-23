130726c4649b10fc97acd91a2aeb8342345bd9d7
ThijsEgberts <t.t.a.egberts@student.tue.nl>
2023-10-23 17:34:42 +0200

    fixed a mistake in the realistic paths

--- run_test_cases.py
+++ run_test_cases.py
@@ -67,64 +67,64 @@ def runSimpleFFPrimitiveGenerator():
     
 # Realistic test cases
 def runRealisticPolyAddSub():
-    solve.solve_exercise("Examples\Simple\Exercises\exercise3.json",
-                         "Examples\Simple\Answers\\answer3.json", False)
-    solve.solve_exercise("Examples\Simple\Exercises\exercise15.json",
-                         "Examples\Simple\Answers\\answer15.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise3.json",
+                         "Examples\Realistic\Answers\\answer3.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise15.json",
+                         "Examples\Realistic\Answers\\answer15.json", False)
 
 def runRealisticPolyMulti():
-    solve.solve_exercise("Examples\Simple\Exercises\exercise13.json",
-                         "Examples\Simple\Answers\\answer13.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise13.json",
+                         "Examples\Realistic\Answers\\answer13.json", False)
 
 def runRealisticPolyLongDivision():
-    solve.solve_exercise("Examples\Simple\Exercises\exercise2.json",
-                         "Examples\Simple\Answers\\answer2.json", False)
-    solve.solve_exercise("Examples\Simple\Exercises\exercise9.json",
-                         "Examples\Simple\Answers\\answer9.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise2.json",
+                         "Examples\Realistic\Answers\\answer2.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise9.json",
+                         "Examples\Realistic\Answers\\answer9.json", False)
 
 def runRealisticPolyEucl():
-    solve.solve_exercise("Examples\Simple\Exercises\exercise11.json",
-                         "Examples\Simple\Answers\\answer11.json", False)
-    solve.solve_exercise("Examples\Simple\Exercises\exercise16.json",
-                         "Examples\Simple\Answers\\answer16.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise11.json",
+                         "Examples\Realistic\Answers\\answer11.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise16.json",
+                         "Examples\Realistic\Answers\\answer16.json", False)
 
 def runRealisticPolyIrreducibilityCheck():
-    solve.solve_exercise("Examples\Simple\Exercises\exercise6.json",
-                         "Examples\Simple\Answers\\answer6.json", False)
-    solve.solve_exercise("Examples\Simple\Exercises\exercise14.json",
-                         "Examples\Simple\Answers\\answer14.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise6.json",
+                         "Examples\Realistic\Answers\\answer6.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise14.json",
+                         "Examples\Realistic\Answers\\answer14.json", False)
 
 def runRealisticPolyIrreducibleGenerator():
-    solve.solve_exercise("Examples\Simple\Exercises\exercise4.json",
-                         "Examples\Simple\Answers\\answer4.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise4.json",
+                         "Examples\Realistic\Answers\\answer4.json", False)
     
 def runRealisticFFAddSub():
-    solve.solve_exercise("Examples\Simple\Exercises\exercise1.json",
-                         "Examples\Simple\Answers\\answer1.json", False)
-    solve.solve_exercise("Examples\Simple\Exercises\exercise10.json",
-                         "Examples\Simple\Answers\\answer10.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise1.json",
+                         "Examples\Realistic\Answers\\answer1.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise10.json",
+                         "Examples\Realistic\Answers\\answer10.json", False)
     
 def runRealisticFFMulti():
-    solve.solve_exercise("Examples\Simple\Exercises\exercise12.json",
-                         "Examples\Simple\Answers\\answer12.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise12.json",
+                         "Examples\Realistic\Answers\\answer12.json", False)
     
 def runRealisticFFdivision():
-    solve.solve_exercise("Examples\Simple\Exercises\exercise0.json",
-                         "Examples\Simple\Answers\\answer0.json", False)
-    solve.solve_exercise("Examples\Simple\Exercises\exercise8.json",
-                         "Examples\Simple\Answers\\answer8.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise0.json",
+                         "Examples\Realistic\Answers\\answer0.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise8.json",
+                         "Examples\Realistic\Answers\\answer8.json", False)
     
 def runRealisticFFinversion():
-    solve.solve_exercise("Examples\Simple\Exercises\exercise5.json",
-                         "Examples\Simple\Answers\\answer5.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise5.json",
+                         "Examples\Realistic\Answers\\answer5.json", False)
     
 def runRealisticFFPrimitivityCheck():
-    solve.solve_exercise("Examples\Simple\Exercises\exercise7.json",
-                         "Examples\Simple\Answers\\answer7.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise7.json",
+                         "Examples\Realistic\Answers\\answer7.json", False)
     
 def runRealisticFFPrimitiveGenerator():
-    solve.solve_exercise("Examples\Simple\Exercises\exercise17.json",
-                         "Examples\Simple\Answers\\answer17.json", False)
+    solve.solve_exercise("Examples\Realistic\Exercises\exercise17.json",
+                         "Examples\Realistic\Answers\\answer17.json", False)
 
 
 # Runs all simple test cases provided by the lecturer
@@ -172,4 +172,4 @@ def runAll():
     runRealistic()
     print("All test cases done.\n")
     
-runSimplePolyAddSub()
+runRealisticPolyAddSub()
