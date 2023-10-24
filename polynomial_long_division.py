a10fd1f678eed991ade0dff2512cc84ca599c3c1
ThijsEgberts <t.t.a.egberts@student.tue.nl>
2023-10-24 16:22:11 +0200

    fixed poly long division for division by a constant

--- polynomial_long_division.py
+++ polynomial_long_division.py
@@ -1,4 +1,4 @@
-from Polynomial import Polynomial
+from Polynomial import Polynomial, createZero
 from polynomial_addition_subtraction import solve_addition_polynomial_arithmetic
 from polynomial_addition_subtraction import solve_subtraction_polynomial_arithmetic
 from polynomial_multiplication import solve_multiplication_polynomial_arithmetic
@@ -15,9 +15,18 @@ def solve_long_division_polynomial_arithmetic(a: Polynomial, b: Polynomial) -> P
     if(b.isZero()):
         raise ZeroDivisionError("Division by zero.")
     
+    #if the divisor is a constant, we just divide all the coefficients of a by b[0]
+    if(len(b.coefficients) == 1):
+        bInv = solve_int_inverse(b.coefficients[0], a.modulo)
+        rCoef = [(i * bInv) % a.modulo for i in a.coefficients]
+        r = Polynomial(a.modulo, rCoef)
+        r.removeLeadingZeroes()
+        return r, createZero(a.modulo)
+    
     # a == b so a/b = a/a = 1
     if(a.coefficients == b.coefficients):
         return Polynomial(a.modulo, [1])
+    
     
     q = Polynomial(a.modulo, [0])
     r = a.copy()
