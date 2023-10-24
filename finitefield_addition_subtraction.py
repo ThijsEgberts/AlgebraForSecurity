bb9bf2be4b47903bcf2b9864717e3b0e1c6b337b
ThijsEgberts <t.t.a.egberts@student.tue.nl>
2023-10-24 16:57:44 +0200

    updated ff add and sub to work with auto tests

--- finitefield_addition_subtraction.py
+++ finitefield_addition_subtraction.py
@@ -3,14 +3,14 @@ from polynomial_addition_subtraction import solve_addition_polynomial_arithmetic
 from polynomial_addition_subtraction import solve_subtraction_polynomial_arithmetic
 from polynomial_long_division import solve_long_division_polynomial_arithmetic
 
-def solve_addition_field_arithmetic(x: Polynomial, y: Polynomial, polynomial_modulo: Polynomial) -> Polynomial:
+def solve_addition_finite_field_arithmetic(x: Polynomial, y: Polynomial, polynomial_modulo: Polynomial) -> Polynomial:
     
     normal = solve_addition_polynomial_arithmetic(x, y)
     remainder = solve_long_division_polynomial_arithmetic(normal, polynomial_modulo)[1]
 
     return remainder
 
-def solve_subtraction_field_arithmetic(x: Polynomial, y: Polynomial, polynomial_modulo: Polynomial) -> Polynomial:
+def solve_subtraction_finite_field_arithmetic(x: Polynomial, y: Polynomial, polynomial_modulo: Polynomial) -> Polynomial:
     
     normal = solve_subtraction_polynomial_arithmetic(x, y)
     remainder = solve_long_division_polynomial_arithmetic(normal, polynomial_modulo)[1]
