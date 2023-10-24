884dd05ede669afa6e9f8d8c1f041cf4379e2bee
ThijsEgberts <t.t.a.egberts@student.tue.nl>
2023-10-24 18:06:46 +0200

    renames ff division to work with the tests

--- finitefield_division.py
+++ finitefield_division.py
@@ -1,7 +1,7 @@
 from Polynomial import Polynomial
 from polynomial_long_division import solve_long_division_polynomial_arithmetic
 
-def solve_addition_field_arithmetic(x: Polynomial, y: Polynomial, polynomial_modulo: Polynomial) -> Polynomial:
+def solve_division_finite_field_arithmetic(x: Polynomial, y: Polynomial, polynomial_modulo: Polynomial) -> Polynomial:
     
     normal = solve_long_division_polynomial_arithmetic(x, y)
     remainder = solve_long_division_polynomial_arithmetic(normal, polynomial_modulo)[1]
